c126dd025ef3ebb00a2df0ac7e2d7b3c8c6bcc4a
jetsada117 <jornny.prombut@gmail.com>
2025-09-25 02:49:50 +0700

    remove unuse

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 import 'package:mobile_delivery/pages/chooserole.dart';
 
 class LoginPage extends StatefulWidget {
