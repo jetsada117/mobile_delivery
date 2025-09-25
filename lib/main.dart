ac5e210c2065ecd7af7e44a88cd7814b16520f85
jetsada117 <jornny.prombut@gmail.com>
2025-09-25 17:51:26 +0700

    initial firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
+import 'package:mobile_delivery/firebase_options.dart';
 import 'package:mobile_delivery/pages/login.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
